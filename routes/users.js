eabfd384c7111826bc91d5693027db83a9355278
Your Name <you@example.com>
2024-09-03 22:56:08 +0530

    Add 3/9

---
+++ routes/users.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;   // Create a new schema
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    require: true,
+    unique: true
+  },
+
+  email: {
+    type: String,
+    require: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    require: true,
+  },
+
+  profilepicture: {
+    type: String,
+    require: true,
+  },
+
+  completedPuzzles: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'PUZZLE',
+  },
+
+  favoritePuzzles: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'PUZZLE',
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+
+})
+
+const USER = mongoose.model('USER', userSchema);     // Create a model from the schema
+module.exports = USER
