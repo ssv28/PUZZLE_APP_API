6ff2a9b840aed65018289fa35bc2ed4781c11ac9
Your Name <you@example.com>
2024-09-03 23:09:31 +0530

    Add 3/9

---
+++ Models/review.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'USER',
+        required: true,
+    },
+
+    puzzle: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'PUZZLE',
+        required: true,
+    },
+
+    commentText: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5, // Assuming a 5-star rating system
+    },
+
+});
+
+
+const REVIEW = mongoose.model('REVIEW', ReviewSchema);
+module.exports = REVIEW;
---
+++ Models/trophy.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TrophySchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  icon: {
+    type: String,
+    trim: true, // URL or file path to the trophy icon
+  },
+
+  earnedBy: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'USER',
+  }],
+ 
+});
+
+
+const TROPHY = mongoose.model('TROPHY', TrophySchema);
+module.exports = TROPHY;
